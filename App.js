60b7b4dcce25ff2f02240dbfa42097fd78e128ec
aleyacare <alejandro.soares@yacare.com>
2022-05-21 10:56:38 -0300

    feat: agregando archivos iniciales

---
+++ App.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+const { templatePath } = require('./helpers/templatePath')
+const log = console.log
+const publicPath = path.resolve(__dirname, 'public')
+
+const app = express()
+
+app.use(express.static(publicPath))
+
+app.listen(3000, () => {
+    log('Run server')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(templatePath('template'))
+})
