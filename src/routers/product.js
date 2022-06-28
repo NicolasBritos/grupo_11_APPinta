5cbbf5431c99de08f473287412c6d37eced0aa75
Carla Diaz <carladaiana4@hotmail.com>
2022-06-28 20:52:10 -0300

    feat: creacion de los productos

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const routers = express.Router()
 const productController = require('../controller/product')
+const multer = require('multer')
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      let filePath = path.join(__dirname, '../../public/img/products')
+      cb(null, filePath)
+    },
+    filename: function(req, file, cb) {
+      let fileName = Date.now() + path.extname(file.originalname)
+      cb(null, fileName)
+    }
+})
+const upload = multer({ storage })
 
 routers.get('/', productController.getAll)
 /*
@@ -9,12 +23,18 @@ routers.get('/', productController.getAll)
 */
 
 
+
+
 routers.get('/:id/update', productController.getUpdate)
 
 routers.put('/:id/update', productController.postUpdate)
 
 routers.get('/create', productController.create)
 
+routers.post('/create', upload.single('image'), productController.postCreate)
+
 routers.get('/:id', productController.getById)
+
+
 
 module.exports = routers
