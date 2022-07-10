20bb80c4a5be54c04fddad6df64c28ae00100b56
aleyacare <alejandro.soares@yacare.com>
2022-07-10 18:55:41 -0300

    refactor: cargar de img y avatar

--- src/routers/product.js
+++ src/routers/product.js
@@ -2,32 +2,20 @@ const express = require('express')
 const routers = express.Router()
 const productController = require('../controller/product')
 const authMiddleware = require('../middlewares/authMiddleware')
-const multer = require('multer')
-const path = require('path')
+const uploadImgMiddleware = require('../middlewares/uploadImgMiddleware')
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      let filePath = path.join(__dirname, '../../public/img/products')
-      cb(null, filePath)
-    },
-    filename: function(req, file, cb) {
-        let fileName = Date.now() + path.extname(file.originalname)
-      cb(null, fileName)
-    }
-})
-const upload = multer({ storage })
 
 routers.get('/', productController.getAll)
 
 routers.get('/create', authMiddleware, productController.create)
 
-routers.post('/create', authMiddleware, upload.single('image'), productController.postCreate)
+routers.post('/create', authMiddleware, uploadImgMiddleware.single('image'), productController.postCreate)
 
 routers.get('/:id', productController.getById)
 
 routers.get('/:id/update', authMiddleware, productController.getUpdate)
 
-routers.put('/:id/update', authMiddleware, upload.single('image'), productController.postUpdate)
+routers.put('/:id/update', authMiddleware, uploadImgMiddleware.single('image'), productController.postUpdate)
 
 routers.delete('/:id/delete', authMiddleware, productController.remove)
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,21 +1,8 @@
 const express = require('express')
 const routers = express.Router()
 const userController = require('../controller/user')
-const path = require('path')
-const multer = require('multer')
+const uploadAvatarMiddleware = require('../middlewares/uploadAvatarMiddleware')
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      let filePath = path.join(__dirname, '../../public/img/users')
-      cb(null, filePath)
-    },
-    filename: function(req, file, cb) {
-      let fileName = Date.now() + path.extname(file.originalname)
-      cb(null, fileName)
-    }
-})
-
-const upload = multer({ storage })
 
 /* LOGIN */
 routers.get('/login', userController.getLogin)
@@ -23,7 +10,7 @@ routers.post('/login', userController.postLogin)
 
 /* REGISTER */
 routers.get('/register', userController.getRegister)
-routers.post('/register', upload.single('avatar'), userController.postRegister)
+routers.post('/register', uploadAvatarMiddleware.single('avatar'), userController.postRegister)
 
 /* FORGOT PASSWORD */
 routers.use('/forgot-password', require('./forgotPassword'))
