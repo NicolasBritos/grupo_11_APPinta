05150b8b77fb093627ab66939c344bd544eeef15
aleyacare <alejandro.soares@yacare.com>
2022-07-21 20:48:22 -0300

    fix: corrigiendo home de usuario logueado

--- src/controller/home.js
+++ src/controller/home.js
@@ -4,7 +4,16 @@ const homeController = {
     
     home: (req, res) => {
         const categories = categoryModel.getAll()
-        const locals = {categories}
+        const locals = {}
+        if (req.session.userLogged) {
+            locals.userLogged = req.session.userLogged
+            locals.isLogged = true
+        } else {
+            locals.userLogged = null
+            locals.isLogged = false
+        }
+
+        locals.categories = categories
         res.render('home', locals)
     }
 }
