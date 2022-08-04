4b7ed7538151d9bf0e3d13d81d19f33160e3a4b1
aleyacare <alejandro.soares@yacare.com>
2022-08-04 08:26:29 -0300

    fix: se arregla remember me del usuario

--- src/middlewares/rememberMeMiddleware.js
+++ src/middlewares/rememberMeMiddleware.js
@@ -1,11 +1,16 @@
 const userModel = require('../models/user');
 
 function rememberMeMiddleware(req, res, next) {
-    if (req.cookies.email != undefined &&
+    
+    if (req.cookies.email !== undefined &&
         req.session.email === undefined) {
         const user = userModel.findByEmail(req.cookies.email)
+        // Se carga en la session asi es tomado por userLoggedMiddleware
         req.session.email = user.email
+        req.session.userLogged = user 
+ 
     }
-     next()
+    
+    next()
 }
 module.exports = rememberMeMiddleware
