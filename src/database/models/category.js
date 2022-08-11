cf894ebd601e45557aabde3366fe7166efab7e89
aleyacare <alejandro.soares@yacare.com>
2022-08-11 19:42:42 -0300

    feat: se agrega modelo de categoria

---
+++ src/database/models/category.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const model = sequelize.define(
+    "Category",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      }
+    }
+  );
+
+  return model;
+};
