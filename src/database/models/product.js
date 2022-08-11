50b3a999b32eaab43103eab040e7b3c7f6e94b48
aleyacare <alejandro.soares@yacare.com>
2022-08-11 19:37:17 -0300

    se agrega models de usuario y producto

---
+++ src/database/models/product.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const model = sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL(7, 2),
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+      },
+      qrCode: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        field: "qr_code"
+      },
+      discount: {
+        type: DataTypes.TINYINT,
+        allowNull: false,
+        defaultValue: 0
+      },
+      img: {
+        type: DataTypes.STRING(500),
+        defaultValue: ""
+      }
+    }
+  );
+
+  return model;
+};
---
+++ src/database/models/user.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const model = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      birthDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: "birth_date"
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+        defaultValue: 2
+      }
+    }
+  );
+
+  return model;
+};
