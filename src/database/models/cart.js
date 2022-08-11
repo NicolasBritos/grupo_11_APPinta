b52ed98f22efbbbeb13bf1eebc4fb55ca262662c
aleyacare <alejandro.soares@yacare.com>
2022-08-11 19:45:47 -0300

    feat: se agrega modelo de carrito

---
+++ src/database/models/cart.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const model = sequelize.define(
+    "Cart",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      discount: {
+        type: DataTypes.TINYINT,
+        allowNull: false,
+        defautlValue: 0
+      },
+      total: {
+        type: DataTypes.DECIMAL(7, 2),
+        allowNull: false,
+      }
+    }
+  );
+
+  return model;
+};
