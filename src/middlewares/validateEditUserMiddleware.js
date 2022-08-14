6b3d8e191d27f3246aa40d96345a815adb5426d9
aleyacare <alejandro.soares@yacare.com>
2022-08-13 21:46:28 -0300

    refactor: se coloca la validacion de las imgs dentro de helpers

--- src/middlewares/validateEditUserMiddleware.js
+++ src/middlewares/validateEditUserMiddleware.js
@@ -4,7 +4,5 @@ const validateImageExtension = require('../helpers/validateImageExtension');
 module.exports = [
   body('name').notEmpty().withMessage('Completar el campo de nombre'),
   body('surname').notEmpty().withMessage('Completar el campo de apellido'),
-  body('email').notEmpty().withMessage('Completar el campo de email').bail().isEmail().withMessage('Escribir un formato de email valido'),
-  body('password').notEmpty().withMessage('Completar el campo de password'),
   body('avatar').custom(validateImageExtension)
 ];
