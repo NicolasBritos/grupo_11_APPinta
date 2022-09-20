23539577809d61c2c6ee1b678afaf3a9049a4fe0
aleyacare <alejandro.soares@yacare.com>
2022-09-20 20:43:54 -0300

    se agrea controller de carito

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -1,15 +1,19 @@
 const cartService = require('../service/cartService')
 
 const cartController = {
+    
     async getCart(req, res) {
         const userId = req.session.userLogged.id
         const cart = await cartService.getUserCart(userId)
+        // FALTA EL CARRITO CON LOS PRODUCTOS
         
         res.render('products/productCart')
     },
+
     addProduct(req, res) {
 
     },
+
     removeProduct(req, res) {
 
     }
