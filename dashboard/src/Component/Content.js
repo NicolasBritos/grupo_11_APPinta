00dd0fc11535b376b25f7ddcb8c0faaa942be07f
aleyacare <alejandro.soares@yacare.com>
2022-09-26 19:16:52 -0300

    se agrega home

--- dashboard/src/Component/Content.js
+++ dashboard/src/Component/Content.js
@@ -1,4 +1,5 @@
 import '../Assets/css/Content.css';
+import Home from './Home.js';
 import Categories from './Categories/Categories.js';
 import NotFound from './NotFound.js';
 import { Routes, Route } from 'react-router-dom';
@@ -7,6 +8,7 @@ function Content() {
   return (
       <div className="Content">
         <Routes>
+        <Route path="/" element={<Home />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
