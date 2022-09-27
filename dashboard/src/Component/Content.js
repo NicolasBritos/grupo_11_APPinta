0dcc3e05a7f74852792635e22f65b9e372019502
Carla Diaz <carladaiana4@hotmail.com>
2022-09-27 20:15:36 -0300

    products parte 2

--- dashboard/src/Component/Content.js
+++ dashboard/src/Component/Content.js
@@ -1,6 +1,7 @@
 import '../Assets/css/Content.css';
 import Home from './Home.js';
 import Categories from './Categories/Categories.js';
+import Products from './Products/Products.js';
 import NotFound from './NotFound.js';
 import { Routes, Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function Content() {
         <Routes>
         <Route path="/" element={<Home />} />
           <Route path="/categories" element={<Categories />} />
+          <Route path="/products" element={<Products />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
 
